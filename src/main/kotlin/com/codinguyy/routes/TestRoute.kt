411a3cf53278dbb9989f450d92d53aaaca9dd7a1
coding-guy <paul.k238@gmail.com>
2022-12-12 13:59:59 +0100

    feat: add route pickup lines

--- src/main/kotlin/com/codinguyy/routes/TestRoute.kt
+++ src/main/kotlin/com/codinguyy/routes/TestRoute.kt
@@ -12,7 +12,21 @@ private val jokes = listOf(
     Jokes("Warum scheint in Ethiopien immer die Sonne? - (...), weil die Kinder immer ihr Teller aufessen"),
     Jokes("Darf ich Ihnen den Stuhl vorschieben? - Gentleman fragt höflich nach Analverkehr"),
     Jokes("Warum scheint in Ethiopien immer die Sonne? - (...), weil die Kinder immer ihr Teller aufessen"),
-    Jokes("Abgemacht ist abgemacht - Stefan, 42, Chirurg")
+    Jokes("Abgemacht ist abgemacht - Stefan, 42, Chirurg"),
+    Jokes("Man muss manchmal ein Arschloch sein, um im Leben weiterzukommen"),
+    Jokes("Meine Oma arbeitet beim FBI, deshalb nennen wir sie auch: Top Sigrid"),
+    Jokes("Wie kannst du mit 14 schon schwanger sein? Chantal - 28, schimpft mit ihrer Tochter."),
+    Jokes("Ein Tag mal abschalten und die Ruhe geniessen, Jochen - 55 - Pfleger auf der Intensivstation"),
+    Jokes("Wie nennt man einen Waschbaer, der viel trainiert? - Waschmaschiene."),
+    Jokes("Habe einem Hipster ins Bein geschossen - Jetzt hopst er. "),
+    Jokes("Was ist der Gott der Vegetarier? - Kraueterbuddah")
+
+)
+
+private val pickuplines = listOf(
+    Jokes("Sag mir deinen Namen und ich sage dir wie du heisst"),
+
+
 )
 
 fun Route.randomJoke() {
@@ -22,5 +36,13 @@ fun Route.randomJoke() {
             jokes.random()
         )
     }
+}
 
+fun Route.randomPickupline() {
+    get("/pickupline") {
+        call.respond(
+            HttpStatusCode.OK,
+            pickuplines.random()
+        )
+    }
 }
